40976bf8ebdb53675c175da4f62dd2c65bcd406d
AyushDhimanIITR <ayushd175@gmail.com>
2023-01-14 02:56:24 +0530

    added login comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-  import './App.css';
+import './App.css';
 import Header from './Components/Header/Header';
+import Signin from './Components/Register/Login';
+import Signup from './Components/Register/Registration';
+// import Form from './Components/Register/Register';
+import {GoogleOAuthProvider} from '@react-oauth/google';
 
 function App() {
   return (
-    <div className="App">
+<GoogleOAuthProvider clientId='715617945900-5d78r00q5ldrduav41u6q9be4sirtukq.apps.googleusercontent.com'>
+
+<div className="App">
+  
       <Header />
+      {/* <Signup /> */}
+      <Signin />
     </div>
+</GoogleOAuthProvider>
   );
 }
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,6 +6,10 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import ContactlessIcon from '@mui/icons-material/Contactless';
+import LoginIcon from '@mui/icons-material/Login';
+import Signup from '../Register/Registration';
+import { Outlet,Link} from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -22,11 +26,15 @@ const Header = () => {
             <MenuIcon />
           </IconButton>
           <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+            <ContactlessIcon sx={{fontSize: 'large' }} />
             R Paisa
           </Typography>
-          <Button><Typography color={"#E6E5DE"}>Login</Typography></Button>
+          
+          <Button><Typography color={"#E6E5DE"}><LoginIcon sx={{fontSize:'large'}}/> Register</Typography></Button>
+
         </Toolbar>
       </AppBar>
+      <Outlet />
     </Box>
   );
 }
